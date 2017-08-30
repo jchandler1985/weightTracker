3ea2765b429aa0df82bbd8989fa54f42b20b5215
jchandler1985 <james.chandler1985@gmail.com>
2017-08-30 20:38:18 +0100

    building out the project

---
+++ Main/Java/WeightTracker.java
@@ -0,0 +1,11 @@
+package weightTracker;
+
+public class WeightTracker{
+	public WeightTracker(int weight, String sessId) {
+		setWeight(weight, sessId);
+	}
+	
+	private setWeight(int w, String s) {
+		// add set weight handling here
+	}
+}
---
+++ Main/Java/weightTrackerSpeechlet.java
@@ -0,0 +1,135 @@
+package weightTracker;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.amazon.speech.slu.Intent;
+import com.amazon.speech.speechlet.IntentRequest;
+import com.amazon.speech.speechlet.LaunchRequest;
+import com.amazon.speech.speechlet.Session;
+import com.amazon.speech.speechlet.SessionEndedRequest;
+import com.amazon.speech.speechlet.SessionStartedRequest;
+import com.amazon.speech.speechlet.SpeechletException;
+import com.amazon.speech.speechlet.SpeechletResponse;
+import com.amazon.speech.ui.PlainTextOutputSpeech;
+import com.amazon.speech.ui.Reprompt;
+import com.amazon.speech.ui.SimpleCard;
+
+public class weightTrackerSpeechlet {
+	private static final Logger log = LoggerFactory.getLogger(weightTrackerSpeechlet.class);
+
+    @Override
+    public void onSessionStarted(final SessionStartedRequest request, final Session session)
+            throws SpeechletException {
+        log.info("onSessionStarted requestId={}, sessionId={}", request.getRequestId(),
+                session.getSessionId());
+        // any initialization logic goes here
+    }
+
+    @Override
+    public SpeechletResponse onLaunch(final LaunchRequest request, final Session session)
+            throws SpeechletException {
+        log.info("onLaunch requestId={}, sessionId={}", request.getRequestId(),
+                session.getSessionId());
+        return getWelcomeResponse();
+    }
+
+    @Override
+    public SpeechletResponse onIntent(final IntentRequest request, final Session session)
+            throws SpeechletException {
+        log.info("onIntent requestId={}, sessionId={}", request.getRequestId(), session.getSessionId());
+        
+        sessID = session.getSessionId();
+        Intent intent = request.getIntent();
+        String intentName = (intent != null) ? intent.getName() : null;
+
+        if ("AddWeight".equals(intentName)) {
+        	weightVal = intent.getSlot("weight");
+        	thisWeight = new WeightTracker(weightVal, sessID);
+        	if(thisWeight.getIsSet() = true) {
+        		return getWeightSetResponse();
+        	} else {
+    			throw new SpeechletException("Could not et weight")
+    		}
+        } else if ("AMAZON.HelpIntent".equals(intentName)) {
+            return getHelpResponse();
+        } else {
+            throw new SpeechletException("Invalid Intent");
+        }
+    }
+
+    @Override
+    public void onSessionEnded(final SessionEndedRequest request, final Session session)
+            throws SpeechletException {
+        log.info("onSessionEnded requestId={}, sessionId={}", request.getRequestId(),
+                session.getSessionId());
+        // any cleanup logic goes here
+    }
+
+    /**
+     * Creates and returns a {@code SpeechletResponse} with a welcome message.
+     *
+     * @return SpeechletResponse spoken and visual response for the given intent
+     */
+    private SpeechletResponse getWelcomeResponse() {
+        String speechText = "Welcome to Weight Tracker, what did you weigh in as today.";
+
+        // Create the Simple card content.
+        SimpleCard card = new SimpleCard();
+        card.setTitle("Weight Tracker");
+        card.setContent(speechText);
+
+        // Create the plain text output.
+        PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+        speech.setText(speechText);
+
+        // Create reprompt
+        Reprompt reprompt = new Reprompt();
+        reprompt.setOutputSpeech(speech);
+
+        return SpeechletResponse.newAskResponse(speech, reprompt, card);
+    }
+
+    /**
+     * Creates a {@code SpeechletResponse} for the hello intent.
+     *
+     * @return SpeechletResponse spoken and visual response for the given intent
+     */
+    private SpeechletResponse getWeightSetResponse() {
+        speechText = "Today's weight has been set to" + thisWeight.getWeight();
+    	    	
+        // Create the Simple card content.
+        SimpleCard card = new SimpleCard();
+        card.setTitle("Weight Tracker");
+        card.setContent(speechText);
+
+        // Create the plain text output.
+        PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+        speech.setText(speechText);
+
+        return SpeechletResponse.newTellResponse(speech, card);
+    }
+
+    /**
+     * Creates a {@code SpeechletResponse} for the help intent.
+     *
+     * @return SpeechletResponse spoken and visual response for the given intent
+     */
+    private SpeechletResponse getHelpResponse() {
+        String speechText = "You can ask me to set your daily weight"
+        // Create the Simple card content.
+        SimpleCard card = new SimpleCard();
+        card.setTitle("Weight Tracker");
+        card.setContent(speechText);
+
+        // Create the plain text output.
+        PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+        speech.setText(speechText);
+
+        // Create reprompt
+        Reprompt reprompt = new Reprompt();
+        reprompt.setOutputSpeech(speech);
+
+        return SpeechletResponse.newAskResponse(speech, reprompt, card);
+    }
+}
